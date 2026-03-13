54b4ca8d1e3ef169526aa12f7ecade2f8812b56d
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-03-13 22:24:09 +0530

    Safe view area implementation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import {Text, View} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello Abhishek 🚀</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Hello Abhishek 🚀</Text>
+        <Text>Hello Abhishek 🚀</Text>
+        <Text>Hello Abhishek 🚀</Text>
+        <Text>Hello Abhishek 🚀</Text>
+      </View>
+    </SafeAreaView>
   );
 }
